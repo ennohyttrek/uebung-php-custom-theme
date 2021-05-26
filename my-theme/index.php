84a9c599f132193e4a01f96fdea8a8b53ed6eed7
Enno Hyttrek <ehy.global@gmail.com>
2021-05-26 09:39:55 +0200

    Initial commit

---
+++ my-theme/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Theme Template</title>
+    <?php wp_head() ?>
+</head>
+
+
+<body>
+
+    <h1>Hello World!</h1>
+</body>
+
+<footer>
+    <?php wp_footer() ?>
+</footer>
+
+</html>
